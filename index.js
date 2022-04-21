c2a4eec49de437252a3500041a9bca6787d101df
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-04-21 01:53:22 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//   console.log(`I'm ${age} years old. Happy birthday to me!`);
+//   debugger;
+// }
+
+function writeCards(arr,eventName){
+  const newArr = []
+  for(let i=0;i<arr.length;i++){
+    
+    newArr[i] = `Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`;
+    
+  }
+  return newArr;
+}
+
+
+function countDown(num){
+  let i=num;
+  while( i >= 0){
+    console.log(i);
+    i--
+  }
+}
